4b99a96b32398d0a6454964df595dbd2b857be4a
g1345468 <bonds-3164@Ocha-2.local>
2015-07-05 02:47:25 +0900

    refs #9 コマンドラインオプションで size オプションが指定された場合の処理を追加した．
    FinderクラスのisTargetメソッドに，grepオプションが指定された場合の処理を追加した．
    ファイルの中身を検索するcheckGrepメソッドを追加した．

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -3,6 +3,8 @@ package com.github.finder;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Finder {
     private Args args;
@@ -29,6 +31,9 @@ public class Finder {
         }
 	if(args.getSize() != null){
             flag &= checkTargetSize(file, args.getSize());
+        }
+	if(args.getGrep() != null){
+            flag &= checkGrep(file, args.getGrep());
         }
 
         return flag;
@@ -79,6 +84,20 @@ public class Finder {
                 return file.length() == size;
             default:
                 // ignore
+            }
+        }
+        return false;
+    }
+    
+    private boolean checkGrep(File file, String pattern){
+        if(file.isFile()){
+            try(BufferedReader in = new BufferedReader(new FileReader(file))){
+                String line;
+                while((line = in.readLine()) != null){
+                    if(line.indexOf(pattern) >= 0){
+                        return true;
+                    }
+                }
             }
         }
         return false;
