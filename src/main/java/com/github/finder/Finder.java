1a8f9424aa772ed93d825dc7e38a2b5c2d0b0384
g1345468 <bonds-3164@Ocha-2.local>
2015-07-05 01:18:29 +0900

    refs #6 コマンドラインオプションで name オプションが指定された場合の処理を追加した．
    FinderクラスのisTargetメソッドを，ファイル名が一致したときtrueを返すように変更した．
    checkTargetNameメソッドを追加した．

---
+++ src/main/java/com/github/finder/Finder.java
@@ -0,0 +1,40 @@
+package com.github.finder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Finder {
+    private Args args;
+
+    public Finder(Args args){
+        this.args = args;
+    }
+
+    public String[] find(String target){
+        List<String> list = new ArrayList<>();
+
+        traverse(list, new File(target));
+
+        return list.toArray(new String[list.size()]);
+    }
+
+    private boolean isTarget(File file){
+	boolean flag = true;
+        if(args.getName() != null){
+            flag &= checkTargetName(file, args.getName());
+        }
+        return flag;
+    }
+
+    private void traverse(List<String> list, File dir){
+        if(isTarget(dir)){
+            list.add(dir.getPath());
+        }
+        if(dir.isDirectory()){
+            for(File file: dir.listFiles()){
+                traverse(list, file);
+            }
+        }
+    }
+}
